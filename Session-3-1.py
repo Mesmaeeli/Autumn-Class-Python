224d5459764d85302777d9fa888b1132c052c7b0
Mesmaeeli <63573547+Mesmaeeli@users.noreply.github.com>
2020-10-14 20:16:16 +0330

    Create Session-3-1.py

---
+++ Session-3-1.py
@@ -0,0 +1,132 @@
+"""in barname dare do
+ta adad migire"""
+"""
+Num6 = int(input('adad aval ro vared konid '))
+Num7 = int(input('adad dovom ro vared  konid '))
+
+#print(Num6 % Num7 == 0)
+
+if Num6 % Num7 == 0:
+   print('bakhshpaziri')
+
+
+
+print('done')
+"""
+
+#Num = int(input('yek adad vared konid '))
+"""
+if Num % 2 == 0:
+   if Num % 4 == 0: 
+      print('bakhshpaziri bar 4')
+   if Num % 4 != 0:
+      print('bakhshpaziri faghat bar 2')
+      
+
+if Num % 2 != 0:
+   print('gher bakhshpazir')
+
+
+
+print('done')
+"""
+"""
+if Num >= 0:
+   print('mosbat')
+
+else:
+   print('manfi')
+"""
+"""
+if Num > 0:
+   print('positive')
+elif Num == 0:
+   print('zero')
+else:
+   print('negative')
+
+print('done')
+"""
+"""
+if 0:
+   print('are')
+
+else:
+   print('na')
+"""
+"""
+l1 = []
+while Num > 0:
+   if Num % 7 == 0:
+      l1.append('hop')
+   else:
+      l1.append(Num)
+   Num -= 1
+"""
+"""
+l2 = [3,4,7,7,8,9,0]
+length = len(l2)
+index = 0
+while index < length:
+   print(l2[index])
+   index += 1
+
+"""
+"""
+str1 = 'salam man mohammad hastam. 27 sal sen daram'
+length = len(str1)
+index = 0
+while index < length:
+   if str1[index] != 'm':
+      print(str1[index])
+   index += 1
+"""
+"""
+l2 = [3,4,7,7,8,9,0]
+length = len(l2)
+index = 0
+while 0 < length:
+   print(l2[length-1])
+   length -= 1
+
+"""
+"""
+l2 = [3,4,7,7,8,9,0]
+for shomarande in l2:
+   print(shomarande)
+
+"""
+"""
+
+for value in [1,2,3]:
+   print('salam')
+"""
+
+
+"""
+l1 = [4,6,3,8,4,8,3,84,12,43,68,10,9,12,4]
+
+for sub in l1:
+   if sub % 3 == 0:
+      print(sub)
+
+index = 0
+length = len(l1)
+while index < length:
+   if l1[index] % 3 == 0:
+      print(l1[index])
+   index += 1
+"""
+"""
+for sub in range(3):
+   print('salam')
+"""
+"""
+Num = 10
+for i in range(3):
+   Num *= Num
+"""   
+
+
+
+
